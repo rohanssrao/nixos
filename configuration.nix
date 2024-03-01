eac55fa4756020f669ae26318b42866ac2ffba48
Rohan Rao <rohanssrao@gmail.com>
2024-03-01 18:06:29 -0500

    convert tabs to spaces

--- configuration.nix
+++ configuration.nix
@@ -3,122 +3,122 @@
 {
   environment.systemPackages = with pkgs; [
 
-		vim
-		git
-		wget
-		curl
-		fish
-		lunarvim
-		xclip
-		gcc
-		eza
-		ripgrep
-		fd
-		fzf
-		bat
-		tldr
-		fastfetch
-		lolcat
-		pv
-		compsize
-		unzip
-		spotdl 
-		tmux
-		podman
-		distrobox
-		wgcf
-		wireguard-tools
+    vim
+    git
+    wget
+    curl
+    fish
+    lunarvim
+    xclip
+    gcc
+    eza
+    ripgrep
+    fd
+    fzf
+    bat
+    tldr
+    fastfetch
+    lolcat
+    pv
+    compsize
+    unzip
+    spotdl 
+    tmux
+    podman
+    distrobox
+    wgcf
+    wireguard-tools
 
-		burpsuite
-		blackbox-terminal
-		gnome.dconf-editor
-		gnome.gnome-tweaks
-		adw-gtk3
-		ddcutil
-		btrfs-assistant
-		vscode
-		vlc
-		lutris
-		calibre
-		drawing
-		pdfarranger
-		video-trimmer
-		switcheroo
-		obs-studio
-		xournalpp
-		haguichi
-		microsoft-edge
-		vesktop
-		obsidian
-		gcolor3
-		meld
-		libreoffice
-		qbittorrent
-		speedcrunch
-		zoom-us
-		virt-manager
-		virtiofsd
-		tor-browser
-		krita
-		gnome-network-displays
+    burpsuite
+    blackbox-terminal
+    gnome.dconf-editor
+    gnome.gnome-tweaks
+    adw-gtk3
+    ddcutil
+    btrfs-assistant
+    vscode
+    vlc
+    lutris
+    calibre
+    drawing
+    pdfarranger
+    video-trimmer
+    switcheroo
+    obs-studio
+    xournalpp
+    haguichi
+    microsoft-edge
+    vesktop
+    obsidian
+    gcolor3
+    meld
+    libreoffice
+    qbittorrent
+    speedcrunch
+    zoom-us
+    virt-manager
+    virtiofsd
+    tor-browser
+    krita
+    gnome-network-displays
 
-		gnomeExtensions.arcmenu
-		gnomeExtensions.appindicator
-		gnomeExtensions.brightness-control-using-ddcutil
-		gnomeExtensions.disable-workspace-animation
-		gnomeExtensions.gesture-improvements
-		gnomeExtensions.just-perfection
-		gnomeExtensions.removable-drive-menu
+    gnomeExtensions.arcmenu
+    gnomeExtensions.appindicator
+    gnomeExtensions.brightness-control-using-ddcutil
+    gnomeExtensions.disable-workspace-animation
+    gnomeExtensions.gesture-improvements
+    gnomeExtensions.just-perfection
+    gnomeExtensions.removable-drive-menu
 
   ];
 
-	programs.firefox = {
-		enable = true;
-		autoConfig =
-			''
-				// skip 1st line
-				try {
-					let cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
-					cmanifest.append('utils');
-					cmanifest.append('chrome.manifest');
-					if(cmanifest.exists()){
-						Components.manager.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
-						ChromeUtils.importESModule('chrome://userchromejs/content/boot.sys.mjs');
-					}
-				} catch(ex) {};
-			'';
-	};
+  programs.firefox = {
+    enable = true;
+    autoConfig =
+      ''
+        // skip 1st line
+        try {
+          let cmanifest = Cc['@mozilla.org/file/directory_service;1'].getService(Ci.nsIProperties).get('UChrm', Ci.nsIFile);
+          cmanifest.append('utils');
+          cmanifest.append('chrome.manifest');
+          if(cmanifest.exists()){
+            Components.manager.QueryInterface(Ci.nsIComponentRegistrar).autoRegister(cmanifest);
+            ChromeUtils.importESModule('chrome://userchromejs/content/boot.sys.mjs');
+          }
+        } catch(ex) {};
+      '';
+  };
 
-	programs.kdeconnect = {
+  programs.kdeconnect = {
     enable = true;
     package = pkgs.gnomeExtensions.gsconnect;
-	};
+  };
 
-	services.snapper = {
-		configs.home = {
-			SUBVOLUME = "/home";
-			TIMELINE_CREATE = true;
-			TIMELINE_CLEANUP = true;
-			TIMELINE_LIMIT_HOURLY = 3;
-			TIMELINE_LIMIT_DAILY = 3;
-			TIMELINE_LIMIT_WEEKLY = 3;
-			TIMELINE_LIMIT_MONTHLY = 0;
-			TIMELINE_LIMIT_YEARLY = 0;
-		};
-	};
+  services.snapper = {
+    configs.home = {
+      SUBVOLUME = "/home";
+      TIMELINE_CREATE = true;
+      TIMELINE_CLEANUP = true;
+      TIMELINE_LIMIT_HOURLY = 3;
+      TIMELINE_LIMIT_DAILY = 3;
+      TIMELINE_LIMIT_WEEKLY = 3;
+      TIMELINE_LIMIT_MONTHLY = 0;
+      TIMELINE_LIMIT_YEARLY = 0;
+    };
+  };
 
-	services.tlp.enable = true;
-	services.power-profiles-daemon.enable = false;
-	# services.logmein-hamachi.enable = true;
+  services.tlp.enable = true;
+  services.power-profiles-daemon.enable = false;
+  # services.logmein-hamachi.enable = true;
 
-	environment.fhs.enable = true;
-	environment.fhs.linkLibs = true;
-	environment.lsb.enable = true;
+  environment.fhs.enable = true;
+  environment.fhs.linkLibs = true;
+  environment.lsb.enable = true;
 
-	virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd.enable = true;
 
-	# Set external display as primary in GDM
-	systemd.tmpfiles.rules = [
+  # Set external display as primary in GDM
+  systemd.tmpfiles.rules = [
     ''f+ /run/gdm/.config/monitors.xml - gdm gdm - <monitors version="2"> <configuration> <logicalmonitor> <x>0</x> <y>0</y> <scale>1</scale> <primary>yes</primary> <monitor> <monitorspec> <connector>HDMI-1</connector> <vendor>LEN</vendor> <product>LEN L23i-18</product> <serial>0x4d473634</serial> </monitorspec> <mode> <width>1920</width> <height>1080</height> <rate>60.000</rate> </mode> </monitor> </logicalmonitor> <disabled> <monitorspec> <connector>eDP-1</connector> <vendor>AUO</vendor> <product>0x20ec</product> <serial>0x00000000</serial> </monitorspec> </disabled> </configuration> </monitors>''
   ];
 
@@ -127,14 +127,14 @@
       ./hardware-configuration.nix # Include the results of the hardware scan.
     ];
 
-	zramSwap.enable = true;
+  zramSwap.enable = true;
 
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-	hardware.i2c.enable = true; # Monitor brightness control
+  hardware.i2c.enable = true; # Monitor brightness control
 
-	hardware.opengl.enable = true;
+  hardware.opengl.enable = true;
 
   users.users.chika = {
     isNormalUser = true;
@@ -154,8 +154,8 @@
       fsType = "btrfs";
       options = [ "subvol=@home" "compress-force=zstd:3" ];
     };
-	
-	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   networking.hostName = "nixos";
 
@@ -178,14 +178,14 @@
   };
 
   services.xserver = {
-		enable = true;
-  	displayManager.gdm.enable = true;
-  	desktopManager.gnome.enable = true;
-  	xkb = {
-			layout = "us";
-			variant = "";
-		};
-	};
+    enable = true;
+    displayManager.gdm.enable = true;
+    desktopManager.gnome.enable = true;
+    xkb = {
+      layout = "us";
+      variant = "";
+    };
+  };
 
   services.printing.enable = true;
 
